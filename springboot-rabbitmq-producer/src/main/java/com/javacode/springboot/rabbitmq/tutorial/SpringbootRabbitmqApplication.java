5f1203d7a5635ed2abcc1568b43de6048afe2ab2
Shashanker_Vaduka <v.shashanker@gmail.com>
2022-04-16 16:01:57 +0530

    Added RabbitMQ Publisher and Consumer Code

---
+++ springboot-rabbitmq-producer/src/main/java/com/javacode/springboot/rabbitmq/tutorial/SpringbootRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.javacode.springboot.rabbitmq.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRabbitmqApplication.class, args);
+	}
+
+}
